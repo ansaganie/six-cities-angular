7ba15c9f2fdaf12c9cd3a06531b9c809d483fca7
Ansagan Islamgali <ansaganie@hotmail.com>
2025-05-21 18:27:06 +0500

    feat: restructure application configuration and routing, remove app.module.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
 import { MainComponent } from './pages/main/main.component';
 import { PropertyComponent } from './pages/property/property.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     component: MainComponent
@@ -16,22 +14,16 @@ const routes: Routes = [
     path: 'offer/:offerId',
     component: PropertyComponent
   },
-  { 
+  {
     path: 'favorites',
     component: FavoritesComponent,
   },
-  { 
+  {
     path: 'login',
     component: LoginComponent
   },
-  { 
+  {
     path: 'logout',
     component: LogoutComponent
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
